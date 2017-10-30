602ba3b4e85c173382b784219e7ac66dbb8dae97
Biruk G <birukbk@gmail.com>
2017-10-30 23:56:49 +0000

    exercise 11 done

--- week1-011.BiggerNumber/src/BiggerNumber.java
+++ week1-011.BiggerNumber/src/BiggerNumber.java
@@ -6,6 +6,11 @@ public class BiggerNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Implement your program here. Remember to ask the input from user
+        System.out.println("Type a number: ");
+        int number1 = reader.nextInt();
+        System.out.println("Type another number");
+        int number2 = reader.nextInt();
+        System.out.println("The bigger number of the two numbers given was: "+ Math.max(number1,number2));
+
     }
 }
