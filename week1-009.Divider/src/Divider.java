cee5697b2331048eb8171baaa2448be8c18fe68f
Biruk G <birukbk@gmail.com>
2017-10-30 23:30:36 +0000

    Adds gitignore

--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -6,6 +6,11 @@ public class Divider {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Implement your program here. Remember to ask the input from user.
+        System.out.println("Type a number: ");
+        double number1 = reader.nextInt();
+        System.out.println("Type another number: ");
+        double number2 = reader.nextInt();
+        double division = number1/number2;
+        System.out.println("Division: "+ number1+ " / " + number2 + " = " + division);
     }
 }
