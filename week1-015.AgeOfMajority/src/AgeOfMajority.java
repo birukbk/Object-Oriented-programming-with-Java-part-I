09d1a231a6a6eeb501ecdf8fe4c11b4859e13494
Biruk G <birukbk@gmail.com>
2017-10-31 22:17:19 +0000

    Exercise 15 Finished.

--- week1-015.AgeOfMajority/src/AgeOfMajority.java
+++ week1-015.AgeOfMajority/src/AgeOfMajority.java
@@ -6,6 +6,14 @@ public class AgeOfMajority {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Type your program here 
+        System.out.println("How old are you?");
+        int age = reader.nextInt();
+        if (age >= 18 ){
+            System.out.println("You have reached the age of maturity!");
+
+        }
+        else {
+            System.out.println("You have not reached the age of maturity");
+        }
     }
 }
