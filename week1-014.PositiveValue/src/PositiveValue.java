fea2ee0b43bdad11acccd42686ca6579241322d3
Biruk G <birukbk@gmail.com>
2017-10-31 22:11:14 +0000

    Exercise 14 complited

--- week1-014.PositiveValue/src/PositiveValue.java
+++ week1-014.PositiveValue/src/PositiveValue.java
@@ -5,7 +5,14 @@ public class PositiveValue {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int number = reader.nextInt();
+        if (number > 0){
+            System.out.println("The number is positive.");
+        }
+        if (number<=0){
+            System.out.println("The number is not positive");
+        }
 
-        // Type your program here:
     }
 }
