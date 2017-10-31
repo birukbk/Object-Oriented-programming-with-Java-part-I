39f3a484b7059b9c2ad3c164b6248ae2a61ac01b
Biruk G <birukbk@gmail.com>
2017-10-31 22:48:03 +0000

    Exercise 18 completed.

--- week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -5,6 +5,25 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type the points[0-60 :");
+        int point = reader.nextInt();
+        int grade = 0;
+        if (point >= 50){
+            grade= 5;
+        }else if (point>=45 && point<=49){
+            grade = 4;
+        }
+        else if (point >= 40 && point<= 44){
+            grade = 3;
+        }else if (point >= 35 && point<= 39){
+            grade = 2;
+        }else if (point >= 30 && point<= 34){
+            grade = 1;
+        }
+        else {
+            System.out.println("Grade: failed");
+        }
+        System.out.println("Grade: "+ grade);
 
     }
 }
