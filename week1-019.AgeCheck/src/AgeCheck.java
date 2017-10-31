8e6b0011804ae2cf3cbf02acca3225c29af567e2
Biruk G <birukbk@gmail.com>
2017-10-31 22:56:54 +0000

    Exercise 19 completed.

--- week1-019.AgeCheck/src/AgeCheck.java
+++ week1-019.AgeCheck/src/AgeCheck.java
@@ -5,6 +5,13 @@ public class AgeCheck {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("How old are you? ");
+        int age = reader.nextInt();
+        if (age >= 0 && age <=120){
+            System.out.println("OK");
+        }else {
+            System.out.println("Impossible!");
+        }
 
     }
 }
