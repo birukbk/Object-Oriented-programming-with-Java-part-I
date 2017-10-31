7075e18c9d2bae9614e8282a690a5431d4bd89b5
Biruk G <birukbk@gmail.com>
2017-10-31 23:23:15 +0000

    Exercise completed.

--- week1-021.LeapYear/src/LeapYear.java
+++ week1-021.LeapYear/src/LeapYear.java
@@ -5,6 +5,17 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type a year: ");
+        int year = reader.nextInt();
+
+        if (year % 4 ==0 && (year % 100 !=0)){
+            System.out.println("The year is a leap year.");
+        }else if (year % 4 ==0 && (year % 100 ==0) && (year % 400 ==0) ){
+            System.out.println("The year is a leap year.");
+        }
+        else {
+            System.out.println("The year is not a leap year.");
+        }
 
     }
 }
