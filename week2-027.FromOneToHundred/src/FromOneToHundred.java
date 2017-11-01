d99dfb69e337929f509d72328b7d0fdfcb4153b0
Biruk G <birukbk@gmail.com>
2017-11-01 22:14:10 +0000

    Exercise 27 complete.

--- week2-027.FromOneToHundred/src/FromOneToHundred.java
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -1,7 +1,11 @@
-
 public class FromOneToHundred {
 
     public static void main(String[] args) {
-        // Write your program here
+        int number = 1;
+        while(number<=100){
+            System.out.println(number);
+            number++;
+        }
+
     }
 }
