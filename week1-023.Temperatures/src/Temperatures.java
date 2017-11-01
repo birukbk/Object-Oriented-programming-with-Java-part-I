7f28cdb0167792a7b4f76030c8c592bd5986142a
Biruk G <birukbk@gmail.com>
2017-11-01 20:39:26 +0000

    Exercise 23 Completed.

--- week1-023.Temperatures/src/Temperatures.java
+++ week1-023.Temperatures/src/Temperatures.java
@@ -6,14 +6,11 @@ public class Temperatures {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        // Write your code here. 
-
-        // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
-        // Remove or comment out these lines above before trying to run the tests.
+        while (true){
+            System.out.println("Enter a number ");
+            double number = Double.parseDouble(reader.nextLine());
+            if (number >= 0 && number <= 39 )
+            Graph.addNumber(number);
+        }
     }
 }
