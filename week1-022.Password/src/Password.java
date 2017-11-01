82b49821ba384197db4e73fd2e993efe739a3ee9
Biruk G <birukbk@gmail.com>
2017-11-01 07:15:04 +0000

    Exercise 22 complete.

--- week1-022.Password/src/Password.java
+++ week1-022.Password/src/Password.java
@@ -7,6 +7,20 @@ public class Password {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
 
-        // Write your code here
+
+        while(true){
+            System.out.println("Type the password: ");
+            String input = reader.nextLine();
+            if (input.equals(password)){
+                System.out.println("Right!");
+                System.out.println("The secret is jerry qbar");
+                break;
+            }else
+            {
+                System.out.println("Wrong!");
+            }
+        }
+
+
     }
 }
