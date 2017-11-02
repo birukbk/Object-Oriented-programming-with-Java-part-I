f4f13c41f85da1c16e5ccc654fcd3f553837715a
Biruk G <birukbk@gmail.com>
2017-11-02 20:52:55 +0000

    Exercise 34 Completed.

--- week2-034.Factorial/src/Factorial.java
+++ week2-034.Factorial/src/Factorial.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        int factorial=1;
+        int counter=1;
+        int until;
+        System.out.println("Type a number :");
+        until = reader.nextInt();
+        while (counter <= until){
+                factorial *=counter;
+                counter++;
+        }
+        System.out.println("Factorial is :"+ factorial);
+
 
     }
 }
