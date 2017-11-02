b558e257f8075596c133b22e83973229d140f915
Biruk G <birukbk@gmail.com>
2017-11-02 21:37:07 +0000

    Exercise completed.

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -5,6 +5,18 @@ public class SumOfThePowers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        int sum=0;
+        double number;
+        int counter =0;
+        System.out.println("Type a number :");
+        number = reader.nextDouble();
+        while (counter<= number){
+            sum += (int)Math.pow(2,counter);
+            counter++;
+
+        }
+        System.out.println("The result is "+ (sum));
 
     }
+
 }
