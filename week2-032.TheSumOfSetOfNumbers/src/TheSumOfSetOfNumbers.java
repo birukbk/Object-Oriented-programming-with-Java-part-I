7acc107626adb349734e19df3e6145677fc51a02
Biruk G <birukbk@gmail.com>
2017-11-02 20:24:44 +0000

    Exercise 32 completed.

--- week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -5,6 +5,18 @@ public class TheSumOfSetOfNumbers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        int number = 0;
+        int untilWhat;
+        int sum=0;
+        System.out.println("Until what :");
+        untilWhat = reader.nextInt();
+        while (number<=untilWhat){
+            sum+=number;
+            number++;
+            System.out.println(sum);
+
+        }
+
 
     }
 }
