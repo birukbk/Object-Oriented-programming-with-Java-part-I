9d2ce1a7cef5e4bc3290ff95df9d34023172a251
Biruk G <birukbk@gmail.com>
2017-11-02 07:17:21 +0000

    Exercise 30 completed.

--- week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -6,8 +6,15 @@ public class UpToCertainNumber {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
-        // Write your code here
-        
+        int upToNumber;
+        int number=1;
+
+        System.out.println("Up to what number: ");
+        upToNumber = reader.nextInt();
+        while (upToNumber>=number){
+            System.out.println(number);
+            number++;
+        }
+
     }
 }
