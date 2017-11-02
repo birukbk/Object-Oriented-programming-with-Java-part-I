2f207820b7d402219fabee5482d1b25ac2e958a2
Biruk G <birukbk@gmail.com>
2017-11-02 20:32:26 +0000

    Exercise 33 Completed.

--- week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        int sum =0;
+        int lowerBound;
+        int upperBound;
+        System.out.println("First: ");
+        lowerBound = reader.nextInt();
+        System.out.println("Last: ");
+        upperBound = reader.nextInt();
+        while (lowerBound <= upperBound){
+            sum += lowerBound;
+            lowerBound++;
+
+        }
+        System.out.println("The sum is : " + sum);
         
     }
 }
