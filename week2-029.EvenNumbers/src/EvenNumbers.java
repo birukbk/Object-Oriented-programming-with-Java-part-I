2c55d880cfd8722855f6aeef555839db9f294439
Biruk G <birukbk@gmail.com>
2017-11-02 07:05:58 +0000

    Exercise 29 completed.

--- week2-029.EvenNumbers/src/EvenNumbers.java
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -2,6 +2,11 @@
 public class EvenNumbers {
 
     public static void main(String[] args) {
-        // Write your code here
+        int number = 2;
+        while (number<=100){
+            if (number%2==0) System.out.println(number);
+            number ++;
+        }
+
     }
 }
