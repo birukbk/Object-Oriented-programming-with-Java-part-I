dde0c75f94d323ff28e7731ab4db7caa4b46df99
Biruk G <birukbk@gmail.com>
2017-11-03 07:14:48 +0000

    Exercise 38 complete.

--- week2-038.ManyPrints/src/ManyPrints.java
+++ week2-038.ManyPrints/src/ManyPrints.java
@@ -2,14 +2,21 @@
 import java.util.Scanner;
 
 public class ManyPrints {
-    // NOTE: do not change the method definition, e.g. add parameters to method
     public static void printText() {
-        // Write your code here
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 
     public static void main(String[] args) {
-        // ask the user how many times the text should be printed
-        // use the while structure to call the printText method several times
+
+        int howMany=0;
         Scanner reader = new Scanner(System.in);
+        System.out.println("How many?");
+        howMany = Integer.parseInt(reader.nextLine());
+        while(0<howMany){
+            printText();
+            howMany--;
+
+        }
+
     }
 }
