1379f5f640894328d5b5ba707e3a089f8bd67e73
Biruk G <birukbk@gmail.com>
2017-11-03 22:02:35 +0000

    Adds completed exercise 40.

--- week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -2,13 +2,15 @@ public class PrintingLikeBoss {
 
     // copy or rewrite the method of Assignment 39.1 here
     public static void printStars(int amount) {
-        int i =0;
-        while (i<amount){
+        int printed = 0;
+        while (printed < amount) {
             System.out.print("*");
-            i++;
+            printed = printed + 1;
         }
-
+        System.out.println("");
     }
+
+
 
     public static void printWhitespaces(int amount) {
         int i =0;
@@ -20,16 +22,35 @@ public class PrintingLikeBoss {
     }
 
     public static void printTriangle(int size) {
+        int i = 1;
+        while (i <= size) {
+            printWhitespaces(size - i);
+            printStars(i);
+            i++;
+        }
         // 40.2
     }
 
     public static void xmasTree(int height) {
         // 40.3
+        int i = 1;
+        while (i <= height) {
+            printWhitespaces(height - i);
+            printStars(i + (i - 1));
+            i++;
+        }
+
+        printWhitespaces(height - 2);
+        printStars(3);
+        printWhitespaces(height - 2);
+        printStars(3);
     }
+
 
     public static void main(String[] args) {
         // Tests do not use main, yo can write code here freely!
         printStars(3);
+        System.out.println("");
         printTriangle(5);
         System.out.println("---");
         xmasTree(4);
