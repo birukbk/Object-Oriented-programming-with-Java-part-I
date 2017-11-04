bb497e505a0ca69ef80086c1a5989318280e38a3
Biruk G <birukbk@gmail.com>
2017-11-04 20:32:34 +0000

    Adds solution to exercise 41.2

--- week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -7,21 +7,27 @@ public class GuessingNumberGame {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
-        int userGuess;
-        System.out.println("Guess a number: ");
-        userGuess = Integer.parseInt(reader.nextLine());
-        if (userGuess==numberDrawn){
-            System.out.println("Congratulations, your guess is correct! ");
-        }else if (userGuess<numberDrawn){
-            System.out.println("The number is Greater");
-        }else {
-            System.out.println("The number is lesser");
+        int userGuess=0;
+
+        while (userGuess!=drawNumber()){
+            System.out.println("Guess a number: ");
+            userGuess = Integer.parseInt(reader.nextLine());
+
+            if(userGuess<numberDrawn){
+                System.out.println("The number is Greater");
+            }else {
+                System.out.println("The number is lesser");
+            }
+        }
+        System.out.println("Congratulations, your guess is correct! ");
+
         }
 
 
+
+
         // program your solution here. Do not touch the above lines!
-        
-    }
+
 
     // DO NOT MODIFY THIS!
     private static int drawNumber() {
