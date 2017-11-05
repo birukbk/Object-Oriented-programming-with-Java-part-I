42a514374eb64512ef13b60d544916a0603afd59
Biruk G <birukbk@gmail.com>
2017-11-05 21:48:22 +0000

    Adds solution to Exercise 46: Average of given numbers.

--- week3-046.AverageOfGivenNumbers/src/AverageOfGivenNumbers.java
+++ week3-046.AverageOfGivenNumbers/src/AverageOfGivenNumbers.java
@@ -1,13 +1,12 @@
 
 public class AverageOfGivenNumbers {
     
-    // implement here again the method of exercise 43
     public static int sum(int number1, int number2, int number3, int number4) {
-        return 0;
+        return number1+number2+number3+number4;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
-        return -1;
+        return sum(number1,number2,number3,number4)/4.0;
     }
 
     public static void main(String[] args) {
