c36dcfe419b9e75800c884a96d1c0447e46f78dc
Biruk G <birukbk@gmail.com>
2017-11-05 20:45:40 +0000

    Adds solution to Exercise 45: Greatest.

--- week3-045.Greatest/src/Greatest.java
+++ week3-045.Greatest/src/Greatest.java
@@ -2,8 +2,11 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-
-        return -1;
+     int largest = Math.max(number1,number2);
+     if (largest>number3){
+         return largest;
+     }else
+         return number3;
     }
 
     public static void main(String[] args) {
