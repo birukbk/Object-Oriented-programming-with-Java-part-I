76665d8166db754fd5d24c6edd0234c69b01d468
Biruk G <birukbk@gmail.com>
2017-11-05 20:23:39 +0000

    Adds solution to Exercise 43: Sum of numbers.

--- week3-043.SumOfNumbers/src/SumOfNumbers.java
+++ week3-043.SumOfNumbers/src/SumOfNumbers.java
@@ -1,8 +1,8 @@
 public class SumOfNumbers {
 
     public static int sum(int number1, int number2, int number3, int number4) {
-        // write your code here
-        return -1;
+
+        return number1 + number2 + number3 + number4;
     }
 
     public static void main(String[] args) {
