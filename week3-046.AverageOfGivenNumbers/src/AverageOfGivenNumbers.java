5650e376780f5805d5b43661196de0b27ea211ba
Biruk G <birukbk@gmail.com>
2017-11-05 14:23:43 +0000

    Adds exercises week3 46 and 47.

---
+++ week3-046.AverageOfGivenNumbers/src/AverageOfGivenNumbers.java
@@ -0,0 +1,17 @@
+
+public class AverageOfGivenNumbers {
+    
+    // implement here again the method of exercise 43
+    public static int sum(int number1, int number2, int number3, int number4) {
+        return 0;
+    }
+
+    public static double average(int number1, int number2, int number3, int number4) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        double result = average(4, 3, 6, 1);
+        System.out.println("Average: " + result);
+    }
+}
---
+++ week3-047.LengthOfName/src/LengthOfName.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class LengthOfName {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // call your method from here
+    }
+    
+    // do here the method
+    // public static int calculateCharacters(String text)
+    
+}
