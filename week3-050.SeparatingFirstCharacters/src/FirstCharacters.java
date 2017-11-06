d255a63761ef67d9b03de1c8da4e3048b9305f2e
Biruk G <birukbk@gmail.com>
2017-11-06 11:12:47 +0000

    Adds solution to Exercise 50: Separating first characters.

--- week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
+++ week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class FirstCharacters {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        if (name.length()<3){
+
+        }else{
+            System.out.println("1. character: "+ name.charAt(0));
+            System.out.println("2. character: "+ name.charAt(1));
+            System.out.println("3. character: "+ name.charAt(2));
+        }
+
+
     }
 }
