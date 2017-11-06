ccdebf5e1d77dbffa95611b90cccd92f082f33bb
Biruk G <birukbk@gmail.com>
2017-11-06 20:12:53 +0000

    adds solution to Exercise 53: First part.

--- week3-053.FirstPart/src/FirstPart.java
+++ week3-053.FirstPart/src/FirstPart.java
@@ -5,5 +5,10 @@ public class FirstPart {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type a word: ");
+        String word = reader.nextLine();
+        System.out.println("Length of the firs part: ");
+        int lengthOfTheFirstPart= reader.nextInt();
+        System.out.println("Result: "+ word.substring(0,lengthOfTheFirstPart));
     }
 }
