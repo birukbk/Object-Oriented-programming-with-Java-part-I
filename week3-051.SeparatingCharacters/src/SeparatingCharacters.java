a459d9f6dc8ed7ccc2697765f9dfea914f886465
Biruk G <birukbk@gmail.com>
2017-11-06 11:22:25 +0000

    Adds solution to Exercise 51: Separating characters.

--- week3-051.SeparatingCharacters/src/SeparatingCharacters.java
+++ week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -5,5 +5,12 @@ public class SeparatingCharacters {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name :");
+        String name = reader.nextLine();
+        int i=0;
+        while (i< name.length()){
+            System.out.println((i+1) + "."+ "character :" + name.charAt(i));
+            i++;
+        }
     }
 }
