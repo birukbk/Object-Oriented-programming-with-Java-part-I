7e1ae8fa6f36397781643233ecc517b121480761
Biruk G <birukbk@gmail.com>
2017-11-06 20:25:10 +0000

    adds solution to Exercise 54: The end part.

--- week3-054.TheEndPart/src/TheEndPart.java
+++ week3-054.TheEndPart/src/TheEndPart.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class TheEndPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type a word: ");
+        String word = reader.nextLine();
+        System.out.println("Length of the end part: ");
+        int lengthOfTheEndPart = reader.nextInt();
+        System.out.println("Result :" + word.substring(word.length()-lengthOfTheEndPart));
     }
 }
