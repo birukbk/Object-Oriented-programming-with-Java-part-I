4fdd27719ce917eceab5ba7da17aeda284fa5f52
Biruk G <birukbk@gmail.com>
2017-11-06 11:32:34 +0000

    Adds solution to Exercise 52: Reversing a name.

--- week3-052.ReversingName/src/ReversingName.java
+++ week3-052.ReversingName/src/ReversingName.java
@@ -3,5 +3,14 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name :");
+        String name = reader.nextLine();
+        String reversedName = "";
+        int i= name.length()-1;
+        while (i>=0){
+            reversedName += name.charAt(i);
+            i--;
+        } System.out.print("In reverse order:"+reversedName );
+
     }
 }
