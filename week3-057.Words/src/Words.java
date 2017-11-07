210b947e12df71595d1814a000f7506ca3f54182
Biruk G <birukbk@gmail.com>
2017-11-07 21:50:26 +0000

    Adds solution to Exercise 57: Words.

--- week3-057.Words/src/Words.java
+++ week3-057.Words/src/Words.java
@@ -5,5 +5,19 @@ public class Words {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
+        String userTypedWord = "";
+
+        do {
+            System.out.println("Type a word :");
+            userTypedWord = reader.nextLine();
+            words.add(userTypedWord);
+        }while(!userTypedWord.isEmpty());
+
+
+        System.out.println("You typed the following words: ");
+        for (String word : words) {
+            System.out.println(word);
+        }
+
     }
 }
