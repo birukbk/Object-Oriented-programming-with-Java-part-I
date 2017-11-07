6c24149ac8782e4f189fcdf54fa307fcfdc00883
Biruk G <birukbk@gmail.com>
2017-11-07 23:01:50 +0000

    Adds solution to Exercise 59: Words in reverse order.

--- week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+
+
+        while (true){
+            System.out.println("Type a word");
+            String input = reader.nextLine();
+            if (input.isEmpty()){
+               break;
+            }else
+            {
+                words.add(input);
+            }
+        }
+        System.out.println("you typed the following words");
+        Collections.reverse(words);
+        for (String word: words) {
+            System.out.println(word);
+        }
     }
 }
