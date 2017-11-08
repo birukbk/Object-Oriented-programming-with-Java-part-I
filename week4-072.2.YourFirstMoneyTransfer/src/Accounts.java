8677f12ed4944a4a90d351dabdc7ac2482aa6749
Biruk G <birukbk@gmail.com>
2017-11-08 22:22:20 +0000

    adds solution to EXERCISE 72.2: YOUR FIRST MONEY TRANSFER.

--- week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
+++ week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
@@ -2,8 +2,12 @@
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account mattsAccount = new Account("Matt's account",1000.0);
+        Account myAccount = new Account("My account", 0.0);
+        mattsAccount.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        System.out.println(myAccount);
+        System.out.println(mattsAccount);
     }
 
 }
