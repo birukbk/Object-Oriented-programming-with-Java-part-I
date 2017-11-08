b344a04425429ec07ef389921a3cd52fb48c2c81
Biruk G <birukbk@gmail.com>
2017-11-08 22:09:34 +0000

    add solution to EXERCISE 72.1: YOUR FIRST ACCOUNT.

--- week4-072.1.YourFirstAccount/src/Accounts.java
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -1,11 +1,10 @@
 
-import java.util.Scanner;
-
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account account = new Account("Barry",100);
+        account.deposit(20);
+        System.out.println(account.toString());
     }
 
 }
