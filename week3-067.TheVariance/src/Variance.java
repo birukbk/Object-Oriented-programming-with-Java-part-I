62f4fa6ddca2e7168468fca93a8b951cbfd5b69a
Biruk G <birukbk@gmail.com>
2017-11-08 20:32:57 +0000

    adds solution to Exercise 67: Variance.

--- week3-067.TheVariance/src/Variance.java
+++ week3-067.TheVariance/src/Variance.java
@@ -3,26 +3,34 @@ import java.util.ArrayList;
 public class Variance {
     // Copy here sum from exercise 63 
     public static int sum(ArrayList<Integer> list) {
-        return 0;
+        int sum = 0;
+        for (int item : list){
+            sum += item;
+        }
+        return sum;
     }
-    
-    // Copy here average from exercise 64 
+
     public static double average(ArrayList<Integer> list) {
-        return 0;
+        int sum = sum(list);
+        return (double) sum / list.size();
     }
 
     public static double variance(ArrayList<Integer> list) {
-        // write code here
-        return 0;
+        double sum = 0;
+        double averageOfList = average(list);
+        for (int item : list ) {
+            sum += Math.pow((item - averageOfList),2);
+        }
+        return sum / (list.size() - 1);
     }
-    
+
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(3);
         list.add(2);
         list.add(7);
         list.add(2);
-        
+
         System.out.println("The variance is: " + variance(list));
     }
 
