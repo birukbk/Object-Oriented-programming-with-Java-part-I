0b377c219ae01bc5f799399e0e2b680d345409d6
Biruk G <birukbk@gmail.com>
2017-11-08 21:21:14 +0000

    adds solution to Exercise 70: Combining ArrayLists.

--- week3-070.CombiningArrayLists/src/CombiningArrayLists.java
+++ week3-070.CombiningArrayLists/src/CombiningArrayLists.java
@@ -3,8 +3,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class CombiningArrayLists {
+    public static void  combine(ArrayList<Integer> first, ArrayList<Integer> second){
+        first.addAll(second);
+
+    }
 
     public static void main(String[] args) {
+
 
         // You can change the values while testing
         ArrayList<Integer> list1 = new ArrayList<Integer>();
@@ -12,18 +17,18 @@ public class CombiningArrayLists {
 
         Collections.addAll(list1, 4, 3);
         // A shorthand for:
-        // list1.add(4);
-        // list1.add(3);
+         list1.add(4);
+         list1.add(3);
 
 
         Collections.addAll(list2, 5, 10, 7);
         // A shorthand for:
-        // list2.add(5);
-        // list2.add(10);
-        // list2.add(7);
+        list2.add(5);
+        list2.add(10);
+        list2.add(7);
 
         // Implement the following method and then remove the comment
-        // combine(list1, list2);
+        combine(list1, list2);
         System.out.println(list1);
         System.out.println(list2);
 
