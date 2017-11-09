c31e2250bef868324aefbf67e73c5fa76173a32a
Biruk G <birukbk@gmail.com>
2017-11-09 06:25:23 +0000

    add solution to EXERCISE 72.3: MONEY TRANSFERS.

--- week4-072.3.MoneyTransfers/src/Accounts.java
+++ week4-072.3.MoneyTransfers/src/Accounts.java
@@ -1,9 +1,16 @@
 
 public class Accounts {
+    public static void transfer(Account from, Account to, double howMuch){
+        from.withdrawal(howMuch);
+        to.deposit(howMuch);
+    }
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account a = new Account("A",100);
+        Account b = new Account("B",0);
+        Account c = new Account("C",0);
+        transfer(a,b,50);
+        transfer(b,c,25);
     }
 
 
