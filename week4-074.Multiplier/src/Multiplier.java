8add2ccdc81da3f013f918300d5a47ce3046be65
Biruk G <birukbk@gmail.com>
2017-11-09 06:54:50 +0000

    add solution to Exercise 74: Multiplier.

---
+++ week4-074.Multiplier/src/Multiplier.java
@@ -0,0 +1,10 @@
+public class Multiplier {
+    private int number;
+    public Multiplier(int number){
+        this.number = number;
+    }
+    public int multiply (int otherNumber){
+        return this.number * otherNumber;
+    }
+
+}
