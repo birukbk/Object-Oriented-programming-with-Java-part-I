4b13c2bffcfdb956cf2af6afdee39ed0c7c81f67
Biruk G <birukbk@gmail.com>
2017-11-09 06:46:17 +0000

    add solution to Exercise 73: Product.

---
+++ week4-073.Product/src/Product.java
@@ -0,0 +1,15 @@
+public class Product {
+    private String nameAtStart;
+    private double priceAtStart;
+    private  int amountAtStart;
+
+    public Product(String nameAtStart, double priceAtStart, int amountAtStart){
+        this.nameAtStart= nameAtStart;
+        this.priceAtStart = priceAtStart;
+        this.amountAtStart = amountAtStart;
+
+    }
+    public void printProduct(){
+        System.out.println(this.nameAtStart + ", " + "price "+this.priceAtStart + ", " + "Amount "+ this.amountAtStart);
+    }
+}
