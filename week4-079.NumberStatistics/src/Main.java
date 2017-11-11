4ce3fa83056934ba3f215167af13d5da31553d01
Biruk G <birukbk@gmail.com>
2017-11-11 23:21:44 +0000

    adds completed solution to Exercise 79: NumberStatistics.

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -1,22 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
-        System.out.println("Amount: " + stats.amountOfNumbers());
-        System.out.println("sum: " + stats.sum());
-        System.out.println("average: " + stats.average());
+        Scanner reader = new Scanner(System.in);
+        NumberStatistics sum = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
 
+        System.out.println("Type numbers: ");
 
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
+        while (true) {
+            int number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            }
 
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
+            sum.addNumber(number);
 
-        // The tests does not work if you do not create the objects in the correct order
+            if (number % 2 == 0) {
+                even.addNumber(number);
+            } else {
+                odd.addNumber(number);
+            }
+        }
 
+        System.out.println("sum: " + sum.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
     }
 }
