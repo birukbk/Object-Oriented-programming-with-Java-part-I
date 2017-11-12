9d256e460f042863aa5a07dbcc6b0e01ce9965d9
Biruk G <birukbk@gmail.com>
2017-11-12 13:51:52 +0000

    add solution to Exercise 81: Password randomizer.

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -1,14 +1,24 @@
 import java.util.Random;
 
 public class PasswordRandomizer {
-    // Define the variables
+    private int length;
 
     public PasswordRandomizer(int length) {
         // Initialize the variable
+        this.length = length;
+
     }
 
     public String createPassword() {
         // write code that returns a randomized password
-        return "";
+        Random random  = new Random();
+        String password = "";
+        int index;
+        for (int i = 0; i < this.length; i++){
+            index = random.nextInt(26);
+            char symbol = "abcdefghijklmnopqrstuvwxyz".charAt(index);
+            password += symbol;
+        }
+        return password;
     }
 }
