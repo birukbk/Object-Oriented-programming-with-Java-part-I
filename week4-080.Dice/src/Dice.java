9519be894b9eeddcae6987d320e4700368796c51
Biruk G <birukbk@gmail.com>
2017-11-12 13:48:49 +0000

    add solution to Exercise 80: Rolling the dice.

--- week4-080.Dice/src/Dice.java
+++ week4-080.Dice/src/Dice.java
@@ -5,11 +5,13 @@ public class Dice {
     private int numberOfSides;
 
     public Dice(int numberOfSides) {
-        // Initialize here the number of sides
+        this.numberOfSides = numberOfSides;
+        random = new Random();
     }
 
     public int roll() {
-        // create here a random number belongig to range 1-numberOfSided
-        return 0;
+        // create here a random number belonging to range 1-numberOfSided
+
+        return 1 + random.nextInt(numberOfSides);
     }
 }
