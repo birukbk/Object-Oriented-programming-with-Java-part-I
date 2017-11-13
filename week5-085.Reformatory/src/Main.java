3d907e07d4e0ef14df07cb520973662ed28a6954
Biruk G <birukbk@gmail.com>
2017-11-13 21:57:12 +0000

    add test code for EXERCISE 85.2: FEEDING A PERSON.

--- week5-085.Reformatory/src/Main.java
+++ week5-085.Reformatory/src/Main.java
@@ -2,6 +2,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write here test code 
+        Reformatory eastHelsinkiReformatory = new Reformatory();
+
+        Person brian = new Person("Brian", 1, 110, 7);
+        Person pekka = new Person("Pekka", 33, 176, 85);
+
+        System.out.println(brian.getName() + " weight: " + eastHelsinkiReformatory.weight(brian) + " kilos");
+        System.out.println(pekka.getName() + " weight: " + eastHelsinkiReformatory.weight(pekka) + " kilos");
+
+        eastHelsinkiReformatory.feed(brian);
+        eastHelsinkiReformatory.feed(brian);
+        eastHelsinkiReformatory.feed(brian);
+
+        System.out.println("");
+
+        System.out.println(brian.getName() + " weight: " + eastHelsinkiReformatory.weight(brian) + " kilos");
+        System.out.println(pekka.getName() + " weight: " + eastHelsinkiReformatory.weight(pekka) + " kilos");
     }
 }
