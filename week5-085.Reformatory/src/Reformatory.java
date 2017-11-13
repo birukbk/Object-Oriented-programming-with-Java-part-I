efd6c60e656c91e1d5c6d444c4afd6ee56dfc8bc
Biruk G <birukbk@gmail.com>
2017-11-13 21:55:35 +0000

    add solution to EXERCISE 85.2: FEEDING A PERSON.

--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -1,9 +1,11 @@
 public class Reformatory {
 
     public int weight(Person person) {
-        // return the weight of the person
-
         return person.getWeight();
+    }
+
+    public void feed(Person person){
+        person.setWeight(person.getWeight()+1);
     }
 
 }
