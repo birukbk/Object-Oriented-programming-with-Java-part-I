72346f579578e4409ceae097cf43a082f15c6b0f
Biruk G <birukbk@gmail.com>
2017-11-13 23:39:53 +0000

    add solution for EXERCISE 86.1: THE "STUPID" LYYRA CARD.

--- week5-086.LyyraCardAndRegister/src/LyyraCard.java
+++ week5-086.LyyraCardAndRegister/src/LyyraCard.java
@@ -16,9 +16,7 @@ public class LyyraCard {
     }
 
     public boolean pay(double amount) {
-       // method checks if the balance of the card is at least amount given as parameter
-       // if not, methods returns false meaning that the card could not be used for the payment
-       // if the balance is enough, the given amount is taken from the balance and true is returned
-        return false;
+        if (this.balance>= amount) return true;
+        else return false;
     }
 }
--- week5-086.LyyraCardAndRegister/src/Main.java
+++ week5-086.LyyraCardAndRegister/src/Main.java
@@ -2,7 +2,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write test code here
+        LyyraCard cardOfPekka = new LyyraCard(10);
+
+        System.out.println("money on the card " + cardOfPekka.balance() );
+        boolean succeeded = cardOfPekka.pay(8);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
+
+        succeeded = cardOfPekka.pay(4);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
     }
 }
 
