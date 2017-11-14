ec4da276dec5d2b9958d3e3aa1c8ecbf36127f81
Biruk G <birukbk@gmail.com>
2017-11-14 20:53:52 +0000

    add solution to EXERCISE 87.2: PRICE DIFFERENCE.

--- week5-087.ApartmentComparison/src/Apartment.java
+++ week5-087.ApartmentComparison/src/Apartment.java
@@ -4,11 +4,13 @@ public class Apartment {
     private int rooms;
     private int squareMeters;
     private int pricePerSquareMeter;
+    private int priceOfApartment;
 
     public Apartment(int rooms, int squareMeters, int pricePerSquareMeter) {
         this.rooms = rooms;
         this.squareMeters = squareMeters;
         this.pricePerSquareMeter = pricePerSquareMeter;
+        this.priceOfApartment = pricePerSquareMeter * squareMeters;
     }
 
     public boolean larger(Apartment otherApartment){
@@ -16,6 +18,14 @@ public class Apartment {
             return true;
         }
         else return false;
+    }
+
+    public int priceDifference(Apartment otherApartment){
+        if (this.priceOfApartment>otherApartment.priceOfApartment){
+            return this.priceOfApartment - otherApartment.priceOfApartment;
+        }
+        else return otherApartment.priceOfApartment - this.priceOfApartment;
+
     }
     
 }
