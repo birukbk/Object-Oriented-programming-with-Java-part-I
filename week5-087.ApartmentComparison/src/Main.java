03e364314b1a4684d5c0b94cbba5dad4df2a9103
Biruk G <birukbk@gmail.com>
2017-11-14 20:44:19 +0000

    add test for EXERCISE 87.1: LARGER.

--- week5-087.ApartmentComparison/src/Main.java
+++ week5-087.ApartmentComparison/src/Main.java
@@ -7,5 +7,8 @@ public class Main {
         Apartment twoRoomsBrooklyn = new Apartment(2, 38, 4200);
         Apartment fourAndKitchenBronx = new Apartment(3, 78, 2500);
 
+        System.out.println( studioManhattan.larger(twoRoomsBrooklyn) );       // false
+        System.out.println( fourAndKitchenBronx.larger(twoRoomsBrooklyn) );   // true
+
     }
 }
