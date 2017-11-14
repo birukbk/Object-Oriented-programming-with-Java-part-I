3d0c5b5d4de71e50835c5e3fe693a2fbebc1ed32
Biruk G <birukbk@gmail.com>
2017-11-14 20:24:28 +0000

    add solution for EXERCISE 86.2: CASH REGISTER AND PAYING WITH CASH.

--- week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -4,28 +4,44 @@ public class CashRegister {
     private double cashInRegister;
     private int economicalSold;
     private int gourmetSold;
-    private double priceOfEconomicalLunch = 2.50;
-    private double priceOfGourmetLunch = 4.0;
 
     public CashRegister() {
         this.cashInRegister = 1000;
+        this.economicalSold=0;
+        this.gourmetSold=0;
         // at start the register has 1000 euros 
     }
 
-    public double payEconomical(double cashGiven) {
-        if (cashGiven>= priceOfEconomicalLunch){
-            this.cashInRegister+= priceOfEconomicalLunch;
+    public boolean payEconomical(LyyraCard card) {
+        if (card.pay(2.50)){
+            this.cashInRegister+= 2.50;
             this.economicalSold++;
-            return cashGiven - priceOfEconomicalLunch;
+            return true;
+        }
+        return false;
+    }
+    public double payEconomical(double cashGiven) {
+        if (cashGiven>=2.50){
+            this.cashInRegister+= 2.50;
+            this.economicalSold++;
+            return cashGiven-2.50;
         }
         return cashGiven;
     }
 
-    public double payGourmet(double cashGiven) {
-        if (cashGiven>= priceOfGourmetLunch){
-            this.cashInRegister+= priceOfGourmetLunch;
+    public boolean payGourmet(LyyraCard card) {
+        if (card.pay(4.0)){
+            this.cashInRegister+= 4.0;
             this.gourmetSold++;
-            return cashGiven - priceOfGourmetLunch;
+            return true;
+        }
+        return false;
+    }
+    public double payGourmet(double cashGiven) {
+        if (cashGiven>=4.0){
+            this.cashInRegister+= 4.0;
+            this.gourmetSold++;
+            return cashGiven-4.0;
         }
         return cashGiven;
     }
@@ -33,5 +49,13 @@ public class CashRegister {
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
+    }
+
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if (sum>0){
+            card.loadMoney(sum);
+            this.cashInRegister += sum;
+        }
+
     }
 }
