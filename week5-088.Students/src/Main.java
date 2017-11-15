1668b731e70c2c21d61dc3717ec46cc74e94a6ea
Biruk G <birukbk@gmail.com>
2017-11-15 06:54:55 +0000

    add test code for EXERCISE 88.1: CLASS STUDENT.

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -1,10 +1,10 @@
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        // write here the main program
+        Student pekka = new Student("Pekka Mikkola", "013141590");
+        System.out.println("name: " + pekka.getName());
+        System.out.println("studentnumber: " + pekka.getStudentNumber());
+        System.out.println(pekka);
     }
 }
