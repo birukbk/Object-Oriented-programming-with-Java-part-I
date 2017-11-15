725b251e6cfaa2485f15d148c397196060f4cfa4
Biruk G <birukbk@gmail.com>
2017-11-15 21:48:55 +0000

    add solution for EXERCISE 90.2: PLAYER.

---
+++ week5-090.TeamAndPlayers/src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    private String playerName;
+    private int amountOfGoal;
+
+    public Player(String playerName, int amountOfGoal){
+        this.playerName = playerName;
+        this.amountOfGoal = amountOfGoal;
+    }
+    public Player(String playerName){
+       this(playerName,0);
+
+    }
+    public String getName(){
+        return this.playerName;
+    }
+    public int goals(){
+        return this.amountOfGoal;
+    }
+    public String toString(){
+        return "Player: " + this.playerName + "," + "goals " + this.amountOfGoal;
+    }
+}
