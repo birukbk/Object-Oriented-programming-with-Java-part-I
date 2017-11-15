8977b01ea4844a75d00d227285195d77edeea9e8
Biruk G <birukbk@gmail.com>
2017-11-15 06:53:20 +0000

    add solution for EXERCISE 88.1: CLASS STUDENT.

---
+++ week5-088.Students/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name;
+    private String studentNumber;
+
+    public Student(String name, String studentNumber) {
+        this.name = name;
+        this.studentNumber = studentNumber;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public String getStudentNumber(){
+        return this.studentNumber;
+    }
+    public String toString(){
+        return this.name + "(" + this.studentNumber +")";
+    }
+}
