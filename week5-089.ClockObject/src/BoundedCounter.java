40e36742778b2f01f1a2ad6f425fc0c844962d8f
Biruk G <birukbk@gmail.com>
2017-11-15 20:55:18 +0000

    add solution to Exercise 89: Clock object.

--- week5-089.ClockObject/src/BoundedCounter.java
+++ week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,41 @@
 
 public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+    // copy here the class BoundedCounter from last weeks assignment 78
+    private  int value=0;
+    private int upperBound;
+
+    public BoundedCounter(int upperBound){
+        this.upperBound = upperBound;
+    }
+
+    public void next(){
+        if (this.value==upperBound){
+            this.value = 0;
+        }
+        else {
+            this.value++;
+
+        }
+
+    }
+
+    public String toString(){
+        if (this.value<10){
+            return  ""+ "0"+this.value;
+        }
+        else {
+            return  ""+ this.value;
+        }
+
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        if (value >= 0 && value <= upperBound)
+            this.value = value;
+    }
+
 }
--- week5-089.ClockObject/src/Clock.java
+++ week5-089.ClockObject/src/Clock.java
@@ -4,15 +4,28 @@ public class Clock {
     private BoundedCounter seconds;
    
     public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
-        // the counters that represent hours, minutes and seconds are created and set to have the correct initial values
+        this.hours = new BoundedCounter(23);
+        this.minutes = new BoundedCounter(59);
+        this.seconds = new BoundedCounter(59);
+
+        this.hours.setValue(hoursAtBeginning);
+        this.minutes.setValue(minutesAtBeginning);
+        this.seconds.setValue(secondsAtBeginning);
+
     }
     
     public void tick() {
-        // Clock advances by one second
+        this.seconds.next();
+        if (this.seconds.getValue()==0){
+            this.minutes.next();
+              if (this.minutes.getValue()==0){
+                  this.hours.next();
+              }
+        }
     }
     
     public String toString() {
         // returns the string representation
-        return "";
+        return this.hours +":"+ this.minutes+":"+ this.seconds;
     }
 }
