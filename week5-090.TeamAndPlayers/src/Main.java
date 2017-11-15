1a8085fc842589e080de8c3e3487e7d1fbd33383
Biruk G <birukbk@gmail.com>
2017-11-15 21:30:46 +0000

    add test code for EXERCISE 90.1: CLASS TEAM.

--- week5-090.TeamAndPlayers/src/Main.java
+++ week5-090.TeamAndPlayers/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // test your code here
 
-        // Team barcelona = new Team("FC Barcelona");
-        // System.out.println("Team: " + barcelona.getName());
+         Team barcelona = new Team("FC Barcelona");
+        System.out.println("Team: " + barcelona.getName());
     }
 }
