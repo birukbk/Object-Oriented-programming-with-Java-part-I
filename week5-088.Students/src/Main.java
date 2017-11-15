b782704316e52ee14ece9ef66e5e3f2efeb2c9df
Biruk G <birukbk@gmail.com>
2017-11-15 07:13:05 +0000

    add solution to EXERCISE 88.2: LIST OF STUDENTS.

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -1,10 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Student pekka = new Student("Pekka Mikkola", "013141590");
-        System.out.println("name: " + pekka.getName());
-        System.out.println("studentnumber: " + pekka.getStudentNumber());
-        System.out.println(pekka);
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<Student>();
+
+
+
+        while (true){
+            System.out.println("Type students name :");
+            String studentName = reader.nextLine();
+            if (studentName.isEmpty()){
+                break;
+            }
+            System.out.println("Type student number: ");
+            String studentNmber = reader.nextLine();
+
+            students.add(new Student(studentName,studentNmber));
+        }
+
+        for (Student student: students
+             ) {
+            System.out.println(student);
+        }
+
     }
 }
