a29ea36926acfc4c9ae01b1ae367b3d3f96b4cfe
Biruk G <birukbk@gmail.com>
2017-11-15 22:08:19 +0000

    add solution for EXERCISE 90.4: THE TEAM MAXIMUM SIZE AND CURRENT SIZE.

--- week5-090.TeamAndPlayers/src/Main.java
+++ week5-090.TeamAndPlayers/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Team barcelona = new Team("FC Barcelona");
+        barcelona.setMaxSize(1);
 
         Player brian = new Player("Brian");
         Player pekka = new Player("Pekka", 39);
-
         barcelona.addPlayer(brian);
         barcelona.addPlayer(pekka);
         barcelona.addPlayer(new Player("Mikael", 1)); // works similarly as the above
 
-        barcelona.printPlayers();
+        System.out.println("Number of players: " + barcelona.size());
     }
 }
--- week5-090.TeamAndPlayers/src/Team.java
+++ week5-090.TeamAndPlayers/src/Team.java
@@ -3,15 +3,17 @@ import java.util.ArrayList;
 public class Team {
     private String name;
     private ArrayList<Player> listOfPlayers = new ArrayList<Player>();
+    int maxTeamSize;
 
     public Team(String name){
         this.name = name;
+        this.maxTeamSize =16;
     }
     public String getName(){
         return this.name;
     }
     public void addPlayer(Player player){
-        listOfPlayers.add(player);
+        if (listOfPlayers.size()< maxTeamSize )  listOfPlayers.add(player);
     }
     public void printPlayers(){
         for (Player player:listOfPlayers
@@ -20,4 +22,12 @@ public class Team {
 
         }
     }
+    public void setMaxSize(int maxsize){
+        this.maxTeamSize = maxsize;
+    }
+
+    public int size(){
+        return listOfPlayers.size();
+    }
+
 }
