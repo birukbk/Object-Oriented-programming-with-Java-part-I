28cd4620d657f4fc71b911f0c6bcae3a3d9823fa
Biruk G <birukbk@gmail.com>
2017-11-16 15:09:10 +0000

    add solution for EXERCISE 91.3: CREATION OF A NEW DATE.

--- week5-091.ExtendingMyDate/src/MyDate.java
+++ week5-091.ExtendingMyDate/src/MyDate.java
@@ -10,6 +10,11 @@ public class MyDate {
         this.month = month;
         this.year = year;
     }
+    public MyDate afterNumberOfDays(int days){
+        MyDate newMyDate = new MyDate(this.day,this.month,this.year);
+        newMyDate.advance(days);
+        return newMyDate;
+    }
 
     public String toString() {
         return this.day + "." + this.month + "." + this.year;
