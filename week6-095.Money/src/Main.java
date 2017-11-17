671f64d60ecd43fb5299f3620559ea8c1eda3386
Biruk G <birukbk@gmail.com>
2017-11-17 23:28:07 +0000

    add test for EXERCISE 95.2: LESS.

--- week6-095.Money/src/Main.java
+++ week6-095.Money/src/Main.java
@@ -3,21 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         Money a = new Money(10,0);
-        Money b = new Money(5,0);
+        Money b = new Money(3,0);
+        Money c = new Money(5,0);
 
-        Money c = a.plus(b);
-
-        System.out.println(a);  // 10.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
-
-        a = a.plus(c);          // NOTE: new Money-object is created and reference to that
-        //           is assigned to variable a.
-        //       The Money object 10.00e that variable a used to hold
-        //           is not referenced anymore
-
-        System.out.println(a);  // 25.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
+        System.out.println(a.less(b));  // false
+        System.out.println(b.less(c));  // true
     }
 }
