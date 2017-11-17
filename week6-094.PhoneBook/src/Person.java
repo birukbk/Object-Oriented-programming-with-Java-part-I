468da5c95dce7808dcb127ec9c5e20b530a5e3e5
Biruk G <birukbk@gmail.com>
2017-11-17 15:53:15 +0000

    re factor EXERCISE 94.1: PERSON add test.

--- week6-094.PhoneBook/src/Person.java
+++ week6-094.PhoneBook/src/Person.java
@@ -1,9 +1,16 @@
 public class Person {
     private String name;
-    private String phoneNumber;
+    private String number;
 
-    public Person(String name, String phoneNumber){
+    public Person(String name, String number){
         this.name = name;
-        this.phoneNumber = phoneNumber;
+        this.number = number;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getNumber(){
+        return this.number;
     }
 }
