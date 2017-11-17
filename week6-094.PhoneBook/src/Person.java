8ea4f5df8a043cc8054bad8694491c4dd4f11d9a
Biruk G <birukbk@gmail.com>
2017-11-17 15:51:14 +0000

    add solution for EXERCISE 94.1: PERSON.

---
+++ week6-094.PhoneBook/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    private String name;
+    private String phoneNumber;
+
+    public Person(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
