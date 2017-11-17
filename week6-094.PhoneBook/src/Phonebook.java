7aec37256340fe4d873d8c624f43c2cd63467e62
Biruk G <birukbk@gmail.com>
2017-11-17 16:04:16 +0000

    add solution to EXERCISE 94.2: ADDING PERSONS TO PHONEBOOK.

---
+++ week6-094.PhoneBook/src/Phonebook.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Phonebook {
+    private ArrayList<Person> phoneBook;
+    
+    public Phonebook(Person person){
+        this.phoneBook.add(person);
+    }
+}
