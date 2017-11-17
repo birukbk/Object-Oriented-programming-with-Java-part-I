f4aa6eea690995517d8e096c9107d399a375e651
Biruk G <birukbk@gmail.com>
2017-11-17 23:52:47 +0000

    add test for EXERCISE 95.3: MINUS.

--- week6-095.Money/src/Main.java
+++ week6-095.Money/src/Main.java
@@ -3,10 +3,20 @@ public class Main {
 
     public static void main(String[] args) {
         Money a = new Money(10,0);
-        Money b = new Money(3,0);
-        Money c = new Money(5,0);
+        Money b = new Money(3,50);
 
-        System.out.println(a.less(b));  // false
-        System.out.println(b.less(c));  // true
+        Money c = a.minus(b);
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 6.50e
+
+        c = c.minus(a);         // NOTE: new Money-object is created and reference to that is assigned to variable c
+        //       the Money object 6.50e that variable c used to hold, is not referenced anymore
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 0.00e
+
     }
 }
