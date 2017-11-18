8caf2b918ffcab92ddab769b5c97ea260f955542
Biruk G <birukbk@gmail.com>
2017-11-18 12:27:37 +0000

    add solution for Exercise 97: Elegant printing of an array.

--- week6-097.ElegantPrinting/src/Main.java
+++ week6-097.ElegantPrinting/src/Main.java
@@ -8,6 +8,10 @@ public class Main {
     }
 
     public static void printElegantly(int[] array) {
-        // Write code here
+        for (int i =0; i < array.length-1; ++i
+             ) {
+            System.out.print(array[i]+ ", ");
+        }
+        System.out.println(array[array.length-1]);
     }
 }
