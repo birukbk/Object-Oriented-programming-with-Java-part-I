c5e9a3e05a673a4af6ac0b956f0c52f6d6623324
Biruk G <birukbk@gmail.com>
2017-11-19 19:48:49 +0000

    add test for EXERCISE 98.2: REVERSE COPY.

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -3,7 +3,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int[] original = {1, 2, 3, 4};
+        int[] reverse = reverseCopy(original);
+
+        // print both
+        System.out.println( "original: " +Arrays.toString(original));
+        System.out.println( "reversed: " +Arrays.toString(reverse));
     }
     public static int[] copy(int[] array){
         int[] copyedArray = new int[array.length];
