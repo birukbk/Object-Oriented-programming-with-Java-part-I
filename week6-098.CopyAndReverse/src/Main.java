d1027193558b21fe5139cdc2639b9460c2bd4e26
Biruk G <birukbk@gmail.com>
2017-11-19 19:36:57 +0000

    add solution for EXERCISE 98.1: COPY.

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
         // write testcode here
     }
+    public static int[] copy(int[] array){
+        int[] copyedArray = new int[array.length];
+        for (int i = 0; i < copyedArray.length; i++){
+            copyedArray[i] = array[i];
+        }
+        return copyedArray;
+    }
     
 }
