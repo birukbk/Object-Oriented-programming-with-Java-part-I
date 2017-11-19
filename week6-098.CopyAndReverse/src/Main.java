be0ccff8aeb50469675ceebb082b90c66eec7676
Biruk G <birukbk@gmail.com>
2017-11-19 19:48:14 +0000

    add solution for EXERCISE 98.2: REVERSE COPY.

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -3,15 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] original = {1, 2, 3, 4};
-        int[] copied = copy(original);
-
-        // change the copied
-        copied[0] = 99;
-
-        // print both
-        System.out.println( "original: " + Arrays.toString(original));
-        System.out.println( "copied: " + Arrays.toString(copied));
+        
     }
     public static int[] copy(int[] array){
         int[] copyedArray = new int[array.length];
@@ -19,6 +11,15 @@ public class Main {
             copyedArray[i] = array[i];
         }
         return copyedArray;
+    }
+
+    public static int[] reverseCopy(int[] array){
+        int[] reversedArray = new int[array.length];
+        for (int i = array.length-1; i >= 0; i--){
+            reversedArray[array.length-1-i] = array[i];
+        }
+        return reversedArray;
+
     }
     
 }
