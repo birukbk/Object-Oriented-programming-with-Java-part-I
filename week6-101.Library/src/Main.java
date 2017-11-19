dbe3b9dae01d9c7ca4f905916c283a682a45e501
Biruk G <birukbk@gmail.com>
2017-11-19 20:50:45 +0000

    add test for EXERCISE 101.2: LIBRARY.

--- week6-101.Library/src/Main.java
+++ week6-101.Library/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
-        System.out.println(cheese.title());
-        System.out.println(cheese.publisher());
-        System.out.println(cheese.year());
+        Library Library = new Library();
 
-        System.out.println(cheese);
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        Library.addBook(cheese);
+
+        Book nhl = new Book("NHL Hockey", "Stanley Kupp", 1952);
+        Library.addBook(nhl);
+
+        Library.addBook(new Book("Battle Axes", "Tom A. Hawk", 1851));
+
+        Library.printBooks();
     }
 }
