219bd43242bf5cb8707d55b95e751ea4d3fd3735
Biruk G <birukbk@gmail.com>
2017-11-19 20:27:25 +0000

    minor edits and amend solution for EXERCISE 100.3: COUNTING THE NUMBER OF STARS.

--- week6-100.NightSky/src/NightSky.java
+++ week6-100.NightSky/src/NightSky.java
@@ -3,19 +3,19 @@ import java.util.Random;
 public class NightSky {
     private double density;
     private int width;
-    private int hight;
+    private int height;
     private int starsInLastPrint = 0;
 
-    public NightSky(double density, int width, int hight){
+    public NightSky(double density, int width, int height){
         this.density = density;
         this.width = width;
-        this.hight = hight;
+        this.height = height;
     }
     public NightSky(double density){
         this(density,20,10);
     }
-    public NightSky(int width, int hight){
-        this(0.1,width,hight);
+    public NightSky(int width, int height){
+        this(0.1,width, height);
     }
     public void printLine(){
         Random random = new Random();
@@ -29,4 +29,15 @@ public class NightSky {
             }
         }
     }
+
+    public void print(){
+        starsInLastPrint = 0;
+        for (int i = 0; i < this.height; i++){
+            printLine();
+        }
+        System.out.println();
+    }
+
+
 }
+
