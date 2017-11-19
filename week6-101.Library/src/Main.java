d83f7f0857d35921ea8e3f31531f8508af0378c1
Biruk G <birukbk@gmail.com>
2017-11-19 20:44:16 +0000

    add test for EXERCISE 101.1: BOOK.

--- week6-101.Library/src/Main.java
+++ week6-101.Library/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        System.out.println(cheese.title());
+        System.out.println(cheese.publisher());
+        System.out.println(cheese.year());
+
+        System.out.println(cheese);
     }
 }
