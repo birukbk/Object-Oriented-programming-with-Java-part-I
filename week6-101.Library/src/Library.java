61310491c13294896dab5d59211f260361f6e88b
Biruk G <birukbk@gmail.com>
2017-11-19 20:59:22 +0000

    add solution for EXERCISE 101.3: SEARCH FUNCTIONALITY.

--- week6-101.Library/src/Library.java
+++ week6-101.Library/src/Library.java
@@ -10,12 +10,43 @@ public class Library {
         listOfBooks.add(newBook);
     }
     public void printBooks(){
-        for (Book book:listOfBooks
-             ) {
+        for (Book book:listOfBooks) {
             System.out.println(book.toString());
 
         }
     }
+
+    public ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book:listOfBooks) {
+            if (book.title().contains(title)){
+                found.add(book);
+            }
+        }
+        return found;
+    }
+    public ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book:listOfBooks) {
+            if (book.publisher().contains(publisher)){
+                found.add(book);
+            }
+        }
+        return found;
+    }
+    public ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book:listOfBooks) {
+            if (book.year()==year){
+                found.add(book);
+            }
+        }
+        return found;
+    }
+
 
 
 }
