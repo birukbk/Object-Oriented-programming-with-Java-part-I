bbd2a1370e5419fb3820264672ef4113a80b18bc
Biruk G <birukbk@gmail.com>
2017-11-19 20:59:41 +0000

    add test for EXERCISE 101.3: SEARCH FUNCTIONALITY.

--- week6-101.Library/src/Main.java
+++ week6-101.Library/src/Main.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Library Library = new Library();
 
-        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
-        Library.addBook(cheese);
-
-        Book nhl = new Book("NHL Hockey", "Stanley Kupp", 1952);
-        Library.addBook(nhl);
-
+        Library.addBook(new Book("Cheese Problems Solved", "Woodhead Publishing", 2007));
+        Library.addBook(new Book("The Stinky Cheese Man and Other Fairly Stupid Tales", "Penguin Group", 1992));
+        Library.addBook(new Book("NHL Hockey", "Stanley Kupp", 1952));
         Library.addBook(new Book("Battle Axes", "Tom A. Hawk", 1851));
 
-        Library.printBooks();
+        ArrayList<Book> result = Library.searchByTitle("Cheese");
+        for (Book book: result) {
+            System.out.println(book);
+        }
+
+        System.out.println("---");
+        for (Book book: Library.searchByPublisher("Penguin Group  ")) {
+            System.out.println(book);
+        }
+
+        System.out.println("---");
+        for (Book book: Library.searchByYear(1851)) {
+            System.out.println(book);
+        }
     }
 }
