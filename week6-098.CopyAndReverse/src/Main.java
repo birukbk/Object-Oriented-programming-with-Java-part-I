953657132ae2cfc48054979c93b1f9a60b01d5af
Biruk G <birukbk@gmail.com>
2017-11-19 19:55:19 +0000

    Minor edit

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         int[] original = {1, 2, 3, 4};
         int[] reverse = reverseCopy(original);
 
-        // print both
         System.out.println( "original: " +Arrays.toString(original));
         System.out.println( "reversed: " +Arrays.toString(reverse));
     }
