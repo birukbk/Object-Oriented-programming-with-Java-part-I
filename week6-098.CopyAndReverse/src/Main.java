4ec029e15868e6e0b47b2b727c1d5bb3b123ec1a
Biruk G <birukbk@gmail.com>
2017-11-19 19:38:07 +0000

    add test for EXERCISE 98.1: COPY.

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,17 @@
-
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write testcode here
+        int[] original = {1, 2, 3, 4};
+        int[] copied = copy(original);
+
+        // change the copied
+        copied[0] = 99;
+
+        // print both
+        System.out.println( "original: " + Arrays.toString(original));
+        System.out.println( "copied: " + Arrays.toString(copied));
     }
     public static int[] copy(int[] array){
         int[] copyedArray = new int[array.length];
