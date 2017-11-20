b8403497e8b54073f7d58cfce2050ef93f4ff2f1
Biruk G <birukbk@gmail.com>
2017-11-20 22:31:05 +0000

    edits Exercise 102: Grade distribution

---
+++ week6-102.GradeDistribution/src/Grade.java
@@ -0,0 +1,8 @@
+public class Grade {
+    private int[] grades;
+
+    public Grade(){
+        this.grades = new int[6];
+    }
+    
+}
--- week6-102.GradeDistribution/src/Main.java
+++ week6-102.GradeDistribution/src/Main.java
@@ -3,20 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        System.out.println("Type exam scores, -1 completes:");
 
-        while (Integer.parseInt(lukija.nextLine())!=-1){
 
-            int grade;
-            if (Integer.parseInt(lukija.nextLine())== -1){
-                break;
-            }
-            grade=Integer.parseInt(lukija.nextLine());
+        System.out.println("Type exam scores, -1 completes: ");
+        int score = Integer.parseInt(lukija.nextLine());
+
+        while (score != -1){
+            score = Integer.parseInt(lukija.nextLine());
         }
-        // implement your program here
-        // do not put all to one method/class but rather design a proper structure to your program
-        
-        // Your program should use only one Scanner object, i.e., it is allowed to call 
-        // new Scanner only once. If you need scanner in multiple places, you can pass it as parameter
+
+
     }
 }
