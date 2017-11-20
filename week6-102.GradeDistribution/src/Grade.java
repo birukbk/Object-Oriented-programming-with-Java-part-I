9b0f9c40a1dfb50817e52bc94bbc85969a818306
Biruk G <birukbk@gmail.com>
2017-11-20 22:32:05 +0000

    add solution for Exercise 102: Grade distribution.

--- week6-102.GradeDistribution/src/Grade.java
+++ week6-102.GradeDistribution/src/Grade.java
@@ -4,5 +4,56 @@ public class Grade {
     public Grade(){
         this.grades = new int[6];
     }
-    
+
+    private int grade(int score){
+        if (score < 30)
+            return 0;
+        if (score < 35)
+            return 1;
+        if (score < 40)
+            return 2;
+        if (score <45)
+            return 3;
+        if (score <50)
+            return 4;
+
+        return 5;
+    }
+
+    public void add(int score){
+        if (score < 0 || score > 60)
+            return;
+
+        int theGrade = grade(score);
+        grades[theGrade] ++;
+    }
+
+    public void print(){
+        System.out.println("Grade distribution: ");
+
+        for (int i = grades.length - 1; i >= 0; i--){
+            System.out.println(i + ": " + printStars(grades[i]));
+        }
+
+        System.out.println("Acdeptance percentage: " + acceptancePercentage());
+    }
+
+    private double acceptancePercentage() {
+        double totalScore = 0;
+        double failed = grades[0];
+        for (int grade : grades) {
+            totalScore += grade;
+        }
+        return 100 * (totalScore - failed) / totalScore;
+    }
+
+    private String printStars(int n){
+        String starContainer = "";
+        for (int i = 0; i < n; i++){
+            starContainer += "*";
+        }
+        return starContainer;
+    }
+
+
 }
--- week6-102.GradeDistribution/src/Main.java
+++ week6-102.GradeDistribution/src/Main.java
@@ -4,14 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
+        Grade gradeDistribution = new Grade();
 
         System.out.println("Type exam scores, -1 completes: ");
         int score = Integer.parseInt(lukija.nextLine());
 
         while (score != -1){
+            gradeDistribution.add(score);
             score = Integer.parseInt(lukija.nextLine());
         }
 
+        gradeDistribution.print();
 
     }
 }
