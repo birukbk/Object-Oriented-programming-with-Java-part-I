0a7436d7a7cde5378f9924514a10bb15015984b8
Biruk G <birukbk@gmail.com>
2017-11-20 22:05:48 +0000

    add solution for first part of Exercise 102: Grade distribution.

--- week6-102.GradeDistribution/src/Main.java
+++ week6-102.GradeDistribution/src/Main.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        System.out.println("Type exam scores, -1 completes:");
+
+        while (Integer.parseInt(lukija.nextLine())!=-1){
+
+            int grade;
+            if (Integer.parseInt(lukija.nextLine())== -1){
+                break;
+            }
+            grade=Integer.parseInt(lukija.nextLine());
+        }
         // implement your program here
         // do not put all to one method/class but rather design a proper structure to your program
         
